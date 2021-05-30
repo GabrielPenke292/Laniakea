1d7d868be796f9b323b3cd78d8e165d9fe9b5cd9
Gabriel Penke <gabri.penke@gmail.com>
2021-05-30 15:56:15 -0300

    Primeiro commit

---
+++ Governo/app/Views/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?= $this->include('templates/head') ?>
+</head>
+<body>  
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <img src="assets/img/governo-logo-sem-fundo.png" alt="Logo do governo">
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="title-top">Governo<br>de<br>Laniakea</div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="menu-top">
+                        <nav>
+                            <ul>
+                                <li>ministérios</li>
+                                <li>notícias</li>
+                            </ul>
+                        </nav>
+                    </div><!-- menu top -->
+                    <button class="btn btn-primary">Entrar</button>
+                </div>
+            </div><!-- row -->
+        </div><!-- container -->
+    </header>
+    
+</body>
+</html>
