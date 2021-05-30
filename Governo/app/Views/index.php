2e611f8f33676590253755ae7949903513d9bce8
Gabriel Penke <gabri.penke@gmail.com>
2021-05-30 19:13:47 -0300

    section boas-vindas e busca

--- Governo/app/Views/index.php
+++ Governo/app/Views/index.php
@@ -4,7 +4,7 @@
     <?= $this->include('templates/head') ?>
 </head>
 <body>  
-    <header>
+    <header class="p-3">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
@@ -18,19 +18,44 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="menu-top">
-                        <nav>
-                            <ul>
-                                <li>ministérios</li>
-                                <li>notícias</li>
+                    <div class="menu-top d-inline-block">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ministérios</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">noticias</a>
+                                </li>
                             </ul>
                         </nav>
                     </div><!-- menu top -->
-                    <button class="btn btn-primary">Entrar</button>
+                    <div class="btn-entrar d-inline-block">
+                        <button class="btn btn-primary ">Entrar</button>
+                    </div>
                 </div>
             </div><!-- row -->
         </div><!-- container -->
     </header>
+
+    <main>
+        <section id="boas-vindas">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-4 text-white msg-bem-vindo text-center">
+                        <h2 class="">Seja bem vindo(a)</h2>
+                        <h4 class="text-suave">O que você procura?</h4>    
+                    </div>
+                </div>
+
+                <div class="row d-flex justify-content-center mt-3">
+                    <div class="col-md-8">
+                        <input class="form-control me-2 search-home" type="search" placeholder="Search" aria-label="Search">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
     
 </body>
 </html>
