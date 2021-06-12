96150ecb5a9b1aa905536ff266a294943bb95099
Gabriel Penke <gabri.penke@gmail.com>
2021-06-12 15:53:43 -0300

    Seção de serviços

--- Banco_Federal/app/Views/pages/index.php
+++ Banco_Federal/app/Views/pages/index.php
@@ -7,8 +7,38 @@
     <?= $this->include('templates/header') ?>
 
     <section id="banner-principal">
-
-    
+        
     </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row mb-3">
+                <div class="col-12">
+                    <div class="services-title text-center p-5 text-white">
+                        <h2>O que podemos fazer por você:</h2>
+                    </div>
+                </div><!-- col -->
+            </div><!-- row -->
+            
+            <div class="row d-flex justify-content-around mb-5">
+                <div class="col-2 bg-secondary service-box" id="service-one">
+                    <p>SERVIÇO 01</p>
+                </div><!-- service one -->
+
+                <div class="col-2 bg-secondary service-box" id="service-two">
+                    <p>SERVIÇO 02</p>
+                </div><!-- service two -->
+
+                <div class="col-2 bg-secondary service-box" id="service-three">
+                    <p>SERVIÇO 03</p>
+                </div><!-- service three -->
+
+                <div class="col-2 bg-secondary service-box" id="service-four">
+                    <p>SERVIÇO 04</p>
+                </div><!-- service four -->
+            </div><!-- row -->
+
+        </div><!-- container -->
+    </section><!-- services -->
 </body>
 </html>
