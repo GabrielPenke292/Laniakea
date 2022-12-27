03c82f76d36671d9bb8c8e68d0a3430667e6bbf4
Gabriel Penke <gabri.penke@gmail.com>
2022-12-27 15:00:56 -0300

    inclusão do template

---
+++ Piloto/app/Views/template/head.php
@@ -0,0 +1,34 @@
+<head>
+  
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
+  <meta name="description" content="GlassM5 Theme Live Demo Site">
+  
+  
+  <title>Laniakea - O projeto</title>
+  <link rel="stylesheet" href="<?= base_url('assets/web/assets/mobirise-icons2/mobirise2.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap-grid.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap-reboot.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/dropdown/css/style.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/socicon/css/styles.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/theme/css/style.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/recaptcha.css') ?>">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Lexend:100,300,400,500,600,700,800&amp;display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lexend:100,300,400,500,600,700,800&amp;display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  <!-- Google Tag Manager-->
+ <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PFK425');</script>
+        <!-- End Google Tag Manager --> 
+  
+
+</head>
