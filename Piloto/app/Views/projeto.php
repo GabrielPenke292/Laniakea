42d5201482c11f6c7d98ca610f9ac8466195648f
Gabriel Penke <gabri.penke@gmail.com>
2023-01-05 21:38:04 -0300

    Refazendo o faq da página 'projeto'

--- Piloto/app/Views/projeto.php
+++ Piloto/app/Views/projeto.php
@@ -27,55 +27,40 @@
                 <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">O que é Laniakea? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
                 <div id="faq1" class="collapse" data-bs-parent=".faq-list">
                     <p>
-                        Um país fictício.
+                        Um país fictício. Existe apenas na cabeça desse humilde dev.
                     </p>
                 </div>
             </li>
 
             <li>
-                <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+                <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">Por que foi criado? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
                 <div id="faq2" class="collapse" data-bs-parent=".faq-list">
                     <p>
-                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                    idéia é criar um ambiente onde diversos projetos possam ser desenvolvidos simulando um grau de complexidade mais próximo do real. Por exemplo: Em Laniakea existe um banco federal, e um sistema de cadastro de pessoas (Como se fosse o registro onde tira certidão de nascimento no Brasil). Uma pessoa pode ser registrada nesse sistema (chamado SLCP, sistema laniaker de cadastro de pessoas) e posteriormente criar uma conta bancária no banco federal. O sistema bancário jamais pode aceitar criar uma conta sem que o DI (Documento de identificação) exista no SLCP.
+                    </p>
+                    <p>
+                    Outros sistemas criados também tem dependência entre sí, criando assim um organismo que gera a complexidade próxima a de um país de verdade.
+                    </p>
+                    <p>
+                    Outro exemplo é o sistema educacional. Em Laniakea existem universidades públicas. Para realizar uma matrícula é necessário o HA (Histórico acadêmico) do ensino básico.
+                    </p>
+                    <p>
+                    Outros sistemas vão sendo implantados com o tempo, e em alguns casos a idéia é burocratizar mesmo. Em um segundo momento surgem novos projetos com recursos similares, mas que visam competir no mesmo segmento, como por exemplo uma startup que cria um banco digital com um processo mais simples de criação de conta (mas sempre respeitando as regras do Banco Federal).
                     </p>
                 </div>
             </li>
 
             <li>
-                <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+                <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">Quando surgiu o projeto? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
                 <div id="faq3" class="collapse" data-bs-parent=".faq-list">
                     <p>
-                        Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                    A idealização do projeto surgiu no começo de 2022, mas o conceito era mais simples, sendo apenas um laboratório de desenvolvimento para meus estudos de programação. Com o tempo a complexidade foi aumentando, mas o projeto permaneceu em ambiente local. No final de 2022, resolvi subir o projeto para um servidor compartilhado para que outras pessoas possam acessar também.
+Nem todos os sistemas do projeto já subiram, e novos sistemas estão sendo criados.
                     </p>
                 </div>
             </li>
 
-            <li>
-                <div data-bs-toggle="collapse" href="#faq4" class="collapsed question">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
-                <div id="faq4" class="collapse" data-bs-parent=".faq-list">
-                    <p>
-                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-                    </p>
-                </div>
-            </li>
-
-            <li>
-                <div data-bs-toggle="collapse" href="#faq5" class="collapsed question">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
-                <div id="faq5" class="collapse" data-bs-parent=".faq-list">
-                    <p>
-                        Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-                    </p>
-                </div>
-            </li>
-
-            <li>
-                <div data-bs-toggle="collapse" href="#faq6" class="collapsed question">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
-                <div id="faq6" class="collapse" data-bs-parent=".faq-list">
-                    <p>
-                        Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
-                    </p>
-                </div>
-            </li>
+            
 
         </ul>
 
