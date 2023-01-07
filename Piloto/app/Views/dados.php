ff39690a58995a59c49fbc4c9249161f38ed06a1
Gabriel Penke <gabri.penke@gmail.com>
2023-01-07 19:47:13 -0300

    Primeiros dados na view de dados (estáticos)

--- Piloto/app/Views/dados.php
+++ Piloto/app/Views/dados.php
@@ -9,6 +9,29 @@
 
 
 <main class="mt-5">
-    <?= $this->include('manutencao'); ?>
+    <?php $manutencao = false; ?>
+    <?php if ($manutencao) { ?>
+        <?= $this->include('manutencao'); ?>
+
+    <?php } else { ?>
+        <div class="container">
+            <div class="row">
+                <h2 class="text-center">Alguns dados oficiais do projeto:</h2>
+            </div>
+
+            <div class="dados-oficiais mt-3">
+                <div class="col-md-4">
+                    <div class="card text-center mb-3" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-size: 45px; color: #eb5d1e;" >45</h5>
+                            <p class="card-text">Pessoas oficialmente registradas.</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    <?php } ?>
 
 </main>
