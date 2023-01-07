d1e491fa996256b2995a9aa1178dde2be178d304
Gabriel Penke <gabri.penke@gmail.com>
2023-01-07 19:36:30 -0300

    atualizando a página de links

--- Piloto/app/Views/links.php
+++ Piloto/app/Views/links.php
@@ -9,6 +9,24 @@
 
 
 <main class="mt-5">
-    <?= $this->include('manutencao'); ?>
+    <?php $manutencao = false; ?>
+    <?php if ($manutencao) { ?>
+        <?= $this->include('manutencao'); ?>
+
+    <?php } else { ?>
+        <div class="container">
+
+            <ol class="list-group list-group-numbered">
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">SLCP</div>
+                        Sistema Laniaker de cadastramento de pessoas
+                    </div>
+                    <a href="https://slcp.laniakea.dev.br">Acessar</a>
+                </li>
+
+            </ol>
+        </div>
+    <?php } ?>
 
 </main>
