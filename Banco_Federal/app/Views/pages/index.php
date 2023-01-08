5bb3f26258834dd54e4a3800e3cb5ff829c992df
Gabriel Penke <gabri.penke@gmail.com>
2023-01-07 21:12:08 -0300

    Troca de template

--- Banco_Federal/app/Views/pages/index.php
+++ Banco_Federal/app/Views/pages/index.php
@@ -1,66 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?= $this->include('templates/head') ?>
-</head>
-<body>
-    <?= $this->include('templates/header') ?>
+<?= $this->extend('template/layout'); ?>
 
-    <section id="banner-principal">
-        
-    </section>
+<?= $this->section('content'); ?>
 
-    <section id="services">
-        <div class="container">
-            <div class="row mb-3">
-                <div class="col-12">
-                    <div class="services-title text-center p-5 text-white">
-                        <h2>O que podemos fazer por você:</h2>
-                    </div>
-                </div><!-- col -->
-            </div><!-- row -->
-            
-            <div class="row d-flex justify-content-around mb-5">
-                <div class="col-2 text-white service-box text-center align-self-center" id="service-one">
-                    <p>Abertura de conta</p>
-                </div><!-- service one -->
-
-                <div class="col-2 text-white service-box" id="service-two">
-                    <p>Crédito</p>
-                </div><!-- service two -->
-
-                <div class="col-2 text-white service-box" id="service-three">
-                    <p>Financiamentos e consórcios</p>
-                </div><!-- service three -->
-
-                <div class="col-2 text-white service-box" id="service-four">
-                    <p>Renegociação de dívidas</p>
-                </div><!-- service four -->
-            </div><!-- row -->
-
-        </div><!-- container -->
-    </section><!-- services -->
-
-    <section id="dual-cards">
-        <div class="row my-4">
-            <div class="col-6">
-                <div class="card-img">
-                
-                </div>
-            </div><!-- col -->
-
-            <div class="col-6">
-                <div class="card-info d-flex h-100 justify-content-center">
-                    <div class="msg align-self-center">
-                        <p>Somos o primeiro banco de Laniakea!</p>
-                        <p>Contem conosco para construir essa incrível nação!</p>
-                    </div>
+<section id="services">
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-12">
+                <div class="services-title text-center p-5 text-white">
+                    <h2>O que podemos fazer por você:</h2>
                 </div>
             </div><!-- col -->
         </div><!-- row -->
-    </section><!-- dual cards -->
 
-    <?= $this->include("templates/footer"); ?>
+        <div class="row d-flex justify-content-around mb-5">
+            <div class="col-2 text-white service-box text-center align-self-center" id="service-one">
+                <p>Abertura de conta</p>
+            </div><!-- service one -->
 
-</body>
-</html>
+            <div class="col-2 text-white service-box" id="service-two">
+                <p>Crédito</p>
+            </div><!-- service two -->
+
+            <div class="col-2 text-white service-box" id="service-three">
+                <p>Financiamentos e consórcios</p>
+            </div><!-- service three -->
+
+            <div class="col-2 text-white service-box" id="service-four">
+                <p>Renegociação de dívidas</p>
+            </div><!-- service four -->
+        </div><!-- row -->
+
+    </div><!-- container -->
+</section><!-- services -->
+
+<section id="dual-cards">
+    <div class="row my-4">
+        <div class="col-6">
+            <div class="card-img">
+
+            </div>
+        </div><!-- col -->
+
+        <div class="col-6">
+            <div class="card-info d-flex h-100 justify-content-center">
+                <div class="msg align-self-center">
+                    <p>Somos o primeiro banco de Laniakea!</p>
+                    <p>Contem conosco para construir essa incrível nação!</p>
+                </div>
+            </div>
+        </div><!-- col -->
+    </div><!-- row -->
+</section><!-- dual cards -->
+
+<?= $this->endSection(); ?>
---
+++ Banco_Federal/app/Views/template/layout.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <?= $this->include('template/head'); ?>
+  <!-- Conteúdo da página -->
+
+  <?= $this->include('template/scripts'); ?>
+
+
+</head>
+
+<body>
+  <!-- View Navbar -->
+  <header class="pt-2">
+    <?= $this->include('template/navbar'); ?>
+    <?= $this->include('template/header'); ?>
+
+  </header>
+
+  <main>
+    <div class="page-content">
+      <!-- Main content -->
+      <div class="content-wrapper">
+        <!-- Inner content -->
+        <div class="content-inner">
+          <?php echo $this->renderSection('content'); ?>
+        </div>
+        <!-- /inner content -->
+      </div>
+      <!-- /main content -->
+    </div>
+
+  </main>
+
+  <footer id="footer">
+    <?= $this->include('template/footer'); ?>
+
+  </footer>
+
+</body>
+
+</html>
