bc267b157b37e68df95e291dd0fdeba8f3bab38a
Gabriel Penke <gabri.penke@gmail.com>
2023-01-08 19:34:59 -0300

    Ajustes no card principal

--- Banco_Federal/app/Views/template/header.php
+++ Banco_Federal/app/Views/template/header.php
@@ -8,8 +8,8 @@
                             <div class="banner_taital">
                                 <h1 class="dance_text">Banco Federal de Laniakea</h1>
                                 <p class="consectetur_text">O primeiro banco do novo país.</p>
-                                <div class="about_bt"><a href="about.html">About Us</a></div>
-                                <div class="contact_bt"><a href="contact.html">Contact Us</a></div>
+                                <div class="about_bt"><a href="about.html">Abrir conta</a></div>
+                                <div class="contact_bt"><a href="contact.html">Já sou cliente</a></div>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -17,43 +17,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="banner_taital">
-                                <h1 class="dance_text">Dance beat</h1>
-                                <p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn</p>
-                                <div class="about_bt"><a href="about.html">About Us</a></div>
-                                <div class="contact_bt"><a href="contact.html">Contact Us</a></div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="image_1"><img src="images/img-1.png"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="banner_taital">
-                                <h1 class="dance_text">Dance beat</h1>
-                                <p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn</p>
-                                <div class="about_bt"><a href="about.html">About Us</a></div>
-                                <div class="contact_bt"><a href="contact.html">Contact Us</a></div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="image_1"><img src="images/img-1.png"></div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
-            <a class="carousel-control-prev" href="#my_slider" role="button" data-slide="prev">
-                <i class="fa fa-left"><img src="images/left-icon.png"></i>
-            </a>
-            <a class="carousel-control-next" href="#my_slider" role="button" data-slide="next">
-                <i class="fa fa-right"><img src="images/right-icon.png"></i>
-            </a>
+
         </div>
     </div>
 </div>
