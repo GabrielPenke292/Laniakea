e7327788e81deed21678468bf62683b2f783a071
Gabriel Penke <gabri.penke@gmail.com>
2023-01-10 21:21:56 -0300

    [BANCO FEDERAL] Banner da página de crédito

--- Banco_Federal/app/Views/pages/credit.php
+++ Banco_Federal/app/Views/pages/credit.php
@@ -2,6 +2,43 @@
 
 <?= $this->section('content'); ?>
 
-<p>Esta é a página de crédito</p>
+<!-- card de pessoa feliz -->
+
+<?= $this->section('headerSection'); ?>
+<div class=" layout_padding">
+    <div class="container-fluid banner_section_credit">
+        <div id="my_slider" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="banner_taital">
+                                <h1 class="dance_text">Banco Federal de Laniakea</h1>
+                                <p class="consectetur_text">O primeiro banco do novo país.</p>
+                                <div class="about_bt"><a href="about.html">Abrir conta</a></div>
+                                <div class="contact_bt"><a href="contact.html">Já sou cliente</a></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <!-- <div class="image_1"><img src="assets/img/contrato.png"></div> -->
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="ram">
+    <div class="border_bg"></div>
+</div>
+<?= $this->endSection(); ?>
+
+<!-- benefícios -->
+
+<!-- documentação necessária -->
+
+<!-- Dados de agência -->
 
 <?= $this->endSection(); ?>
