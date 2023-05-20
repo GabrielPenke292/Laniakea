4424cf94f8d0b3357a3b47bdcf37cde01466dfbc
Gabriel Penke <gabri.penke@gmail.com>
2023-05-20 18:42:53 -0300

    style: ajustes na sidebar e background color do body

--- Banco_Federal/app/Views/template-client/sidebar.php
+++ Banco_Federal/app/Views/template-client/sidebar.php
@@ -10,7 +10,7 @@
                 </div>
             </div>
             <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#abou">&euro; Transferir</a></li>
             <li><a href="#events">Events</a></li>
             <li><a href="#team">Team</a></li>
             <li class="dropdown">
