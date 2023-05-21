4688fb83ca30a72cd15a7e9ad72d113bf3c68e1a
Gabriel Penke <gabri.penke@gmail.com>
2023-05-21 19:08:39 -0300

    style: correção na exibição da unidade monetária

--- Banco_Federal/app/Views/client/components/saldo.php
+++ Banco_Federal/app/Views/client/components/saldo.php
@@ -2,7 +2,7 @@
     <img src="assets/img/client/money.svg" class="card-img-top pt-2" alt="saldo" id="icon-home">
     <div class="card-body">
         <h5 class="card-title">Saldo</h5>
-        <h3 class="card-text">R$ 5320,10</h3>
+        <h3 class="card-text">$ 5320,10</h3>
         <a href="<?php echo base_url("extract");  ?>" class="btn btn-primary">Extrato</a>
     </div>
 </div>
