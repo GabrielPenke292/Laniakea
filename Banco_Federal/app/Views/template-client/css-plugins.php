555302b4e37ebd6f8a09a6699d274b2a8bd88c08
Gabriel Penke <gabri.penke@gmail.com>
2023-05-24 21:13:16 -0300

    feat: instalação bootstrap-icons

--- Banco_Federal/app/Views/template-client/css-plugins.php
+++ Banco_Federal/app/Views/template-client/css-plugins.php
@@ -3,3 +3,5 @@
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/bootstrap-clockpicker.min.css')?>">
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/bootstrap-material-datetimepicker.css')?>">
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/daterangepicker.css')?>">
+<link rel="stylesheet" href="<?= base_url('assets/plugins/bootstrap-icons/css/bootstrap-icons.css')?>">
+
