b64ea847864cba89e33864d65161f252453ff42c
Gabriel Penke <gabri.penke@gmail.com>
2023-05-24 21:23:25 -0300

    style: ajuste no tamanho do ícone de visualizar o extrato individual

--- Banco_Federal/app/Views/template-client/css-plugins.php
+++ Banco_Federal/app/Views/template-client/css-plugins.php
@@ -3,5 +3,6 @@
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/bootstrap-clockpicker.min.css')?>">
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/bootstrap-material-datetimepicker.css')?>">
 <link rel="stylesheet" href="<?= base_url('assets/plugins/datepicker/css/daterangepicker.css')?>">
-<link rel="stylesheet" href="<?= base_url('assets/plugins/bootstrap-icons/css/bootstrap-icons.css')?>">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
 
