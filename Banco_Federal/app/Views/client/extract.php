02d23468bcece4c57442e9a85ed3018a3fe6886c
Gabriel Penke <gabri.penke@gmail.com>
2023-05-27 17:53:25 -0300

    style: ajustes no visual da página de extrato

--- Banco_Federal/app/Views/client/extract.php
+++ Banco_Federal/app/Views/client/extract.php
@@ -31,9 +31,7 @@
                             <td>14/05/2023 14:12:26</td>
                             <td>$ 12,98</td>
                             <td>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                                    <i class="bi bi-eye-fill text-primary" id="icone-visualizar-extrato" data-toggle="modal" data-target="#exampleModal"></i>
-                                </button>
+                                <i class="bi bi-eye-fill text-primary cursor-pointer" id="icone-visualizar-extrato" onclick="app.modal('extrato-movimento', 1)"></i>
                             </td>
                         </tr>
 
@@ -66,54 +64,45 @@
 
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+        <div class="modal fade" id="extrato-movimento" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <div class="modal-header bg-secondary">
+                        <h5 class="modal-title text-white" id="exampleModalLabe">Movimento</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+                            <span aria-hidden="true" class="text-white">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <div class="row">
+                            <div class="col-md-5">
+                                <p>Data: 14/05/2023</p>
+                                <p>Hora: 09:45:29</p>
+                                <p>Valor: $12,98</p>
+                                <p>Modalidade: Transferência bancária</p>
+                            </div>
+                            <div class="col-md-2 text-center">
+                                <hr class="my-auto mx-3" style="height: 100%; width: 4px; background-color: green;">
+                            </div>
+                            <div class="col-md-5">
+                                <p>Agência: 0001</p>
+                                <p>Conta: 00000000000000000000</p>
+                                <p>CPF: 000.000.000-00</p>
+                                <p>Nome: Nome da pessoa/estabelecimento</p>
+                                <p>Telefone: (00) 00000-0000</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
                     </div>
                 </div>
             </div>
         </div>
 </div>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 
 </main>
 
+<script src="<?php echo base_url('assets/js/client/extract.js') ?>"></script>
 <?= $this->endSection(); ?>
 </div>
