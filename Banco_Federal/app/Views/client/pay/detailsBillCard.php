6704258aa3440e36ba2e5b2e7a1180f9d052a62a
Gabriel Penke <gabri.penke@gmail.com>
2023-06-08 22:08:03 -0300

    feat: ajustes no card de detalhes de pagamento

--- Banco_Federal/app/Views/client/pay/detailsBillCard.php
+++ Banco_Federal/app/Views/client/pay/detailsBillCard.php
@@ -1,8 +1,9 @@
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Fatura ${bill.id}</h5>
-        <p class="card-text">Descrição: ${bill.description}</p>
-        <p class="card-text">Valor: ${bill.value}</p>
-        <p class="card-text">Vencimento: ${bill.due_date}</p>
+        <h5 class="card-title">Fatura <?= $idBill ?></h5>
+        <p class="card-text">Descrição: <?= $description ?></p>
+        <p class="card-text">Valor: <?= $value ?></p>
+        <p class="card-text">Vencimento: <?= $due_date ?></p>
+        <p class="card-text">Esse débito foi gerado no dia: 05/06/2023</p>
     </div>
 </div>
