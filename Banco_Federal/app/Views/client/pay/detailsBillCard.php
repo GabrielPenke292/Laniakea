0126bfa7f13ce8b3fb46a44627219076b6deef6c
Gabriel Penke <gabri.penke@gmail.com>
2023-06-09 19:41:16 -0300

    style: botões de pagar e cancelar e ajustes na row

--- Banco_Federal/app/Views/client/pay/detailsBillCard.php
+++ Banco_Federal/app/Views/client/pay/detailsBillCard.php
@@ -1,9 +1,20 @@
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Fatura <?= $idBill ?></h5>
-        <p class="card-text">Descrição: <?= $description ?></p>
-        <p class="card-text">Valor: <?= $value ?></p>
-        <p class="card-text">Vencimento: <?= $due_date ?></p>
-        <p class="card-text">Esse débito foi gerado no dia: 05/06/2023</p>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <h5 class="card-title">Fatura <?= $idBill ?></h5>
+                <p class="card-text">Descrição: <?= $description ?></p>
+                <p class="card-text">Valor: <?= $value ?></p>
+                <p class="card-text">Vencimento: <?= $due_date ?></p>
+                <p class="card-text">Esse débito foi gerado no dia: 05/06/2023</p>
+            </div>
+            <div class="col-md-4 col-sm-6 d-flex align-items-stretch ">
+                <button type="button" class="btn btn-primary w-50" onclick="app.pay_bill(<?= $idBill ?>)">Pagar</button>
+            </div>
+            <div class="col-md-4 col-sm-6 d-flex align-items-stretch">
+                <button type="button" class="btn btn-danger w-50" onclick="app.cancel_bill(<?= $idBill ?>)">Cancelar</button>
+            </div>
+        </div>
+
     </div>
 </div>
