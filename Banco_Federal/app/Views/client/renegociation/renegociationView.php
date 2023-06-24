5440d5752e30ed0905ac1120b714b7e95e492dac
Gabriel Penke <gabri.penke@gmail.com>
2023-06-24 19:00:23 -0300

    style: outros detalhes da pagina de renegociação

--- Banco_Federal/app/Views/client/renegociation/renegociationView.php
+++ Banco_Federal/app/Views/client/renegociation/renegociationView.php
@@ -26,26 +26,31 @@
         <div class="col-12 text-center">
             <h2>Opções de Renegociação</h2>
         </div>
-        <ul>
-            <li><a href="#opcao1">Renegociar parcelas</a></li>
-            <li><a href="#opcao2">Revisão de juros</a></li>
-            <li><a href="#opcao3">Alteração de prazo</a></li>
-            <li><a href="#opcao4">Pagamento único com desconto</a></li>
-        </ul>
+        <div class="col-12">
+            <ul class="list-group">
+                <li><button type="button" class="list-group-item btn btn-outline-primary mb-2 disabled">Renegociar Parcelas</button></li>
+                <li><button type="button" class="list-group-item btn btn-outline-primary mb-2 disabled">Revisão de juros</button></li>
+                <li><button type="button" class="list-group-item btn btn-outline-primary mb-2 disabled">Alteração de prazo</button></li>
+                <li><button type="button" class="list-group-item btn btn-outline-primary mb-2 disabled">Pagamento único com desconto</button></li>
+            </ul>
+        </div>
+        <hr>
+        <div class="renegociacao-sumario container-fluid card p-3">
+            <h2 id="opcao1">Renegociar parcelas</h2>
+            <p>A opção de renegociar parcelas permite ajustar o valor e a quantidade de parcelas a serem pagas. Você pode definir novos valores mensais e a duração total da renegociação.</p>
+    
+            <h2 id="opcao2">Revisão de juros</h2>
+            <p>A revisão de juros possibilita a análise e a possibilidade de redução das taxas de juros aplicadas à sua dívida. Você pode solicitar uma revisão para diminuir os encargos financeiros.</p>
+    
+            <h2 id="opcao3">Alteração de prazo</h2>
+            <p>Com a opção de alteração de prazo, você pode estender ou encurtar o período de pagamento da sua dívida. Ajuste a quantidade de meses para se adequar à sua capacidade financeira.</p>
+    
+            <h2 id="opcao4">Pagamento único com desconto</h2>
+            <p>A opção de pagamento único com desconto permite que você quite a sua dívida em uma única parcela, com um desconto especial aplicado. Essa é uma alternativa para quem deseja eliminar a dívida de uma vez.</p>
+    
+            <p>Selecione a opção de renegociação que melhor atende às suas necessidades e entre em contato com um de nossos atendentes para obter mais informações e iniciar o processo de renegociação.</p>
 
-        <h2 id="opcao1">Renegociar parcelas</h2>
-        <p>A opção de renegociar parcelas permite ajustar o valor e a quantidade de parcelas a serem pagas. Você pode definir novos valores mensais e a duração total da renegociação.</p>
-
-        <h2 id="opcao2">Revisão de juros</h2>
-        <p>A revisão de juros possibilita a análise e a possibilidade de redução das taxas de juros aplicadas à sua dívida. Você pode solicitar uma revisão para diminuir os encargos financeiros.</p>
-
-        <h2 id="opcao3">Alteração de prazo</h2>
-        <p>Com a opção de alteração de prazo, você pode estender ou encurtar o período de pagamento da sua dívida. Ajuste a quantidade de meses para se adequar à sua capacidade financeira.</p>
-
-        <h2 id="opcao4">Pagamento único com desconto</h2>
-        <p>A opção de pagamento único com desconto permite que você quite a sua dívida em uma única parcela, com um desconto especial aplicado. Essa é uma alternativa para quem deseja eliminar a dívida de uma vez.</p>
-
-        <p>Selecione a opção de renegociação que melhor atende às suas necessidades e entre em contato com um de nossos atendentes para obter mais informações e iniciar o processo de renegociação.</p>
+        </div>
 
     </main>
 
