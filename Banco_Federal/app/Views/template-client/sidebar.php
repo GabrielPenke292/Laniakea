c2bf8e1cc461c6f0c7a338ec82afc20ec10d979a
Gabriel Penke <gabri.penke@gmail.com>
2023-10-30 22:44:33 -0300

    fix: ajuste no link da home na sidebar

--- Banco_Federal/app/Views/template-client/sidebar.php
+++ Banco_Federal/app/Views/template-client/sidebar.php
@@ -1,81 +1,81 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="sidebar-sticky pt-3">
-        <ul class="nav flex-column ">
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'home' ? 'active' : ''  ?>" href="<?php echo base_url("client"); ?>">
-              <span data-feather="home"></span>
-              Início <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'extract' ? 'active' : ''  ?>" href="<?php echo base_url("client/extract");  ?>">
-              <span data-feather="file"></span>
-              Extrato
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'transfer' ? 'active' : ''  ?>" href="<?php echo base_url("client/transfer"); ?>">
-              <span data-feather="shopping-cart"></span>
-              Transferência
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'pay' ? 'active' : ''  ?>" href="<?php echo base_url("client/pay"); ?>">
-              <span data-feather="shopping-cart"></span>
-              Pagar
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'investments' ? 'active' : ''  ?>" href="<?php echo base_url('client/investments'); ?> ">
-              <span data-feather="users"></span>
-              Investimentos
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'loan' ? 'active' : ''  ?>" href="<?php echo base_url('client/loans'); ?>">
-              <span data-feather="bar-chart-2"></span>
-              Empréstimo
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'card' ? 'active' : ''  ?>" href="<?php echo base_url('client/card'); ?>">
-              <span data-feather="layers"></span>
-              Cartão
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link <?= $page == 'renegociation' ? 'active' : ''  ?>" href="<?php echo base_url('client/renegociation') ?>">
-              <span data-feather="layers"></span>
-              Renegociação
-            </a>
-          </li>
-        </ul>
+  <div class="sidebar-sticky pt-3">
+    <ul class="nav flex-column ">
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'home' ? 'active' : ''  ?>" href="<?php echo base_url("client/home"); ?>">
+          <span data-feather="home"></span>
+          Início <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'extract' ? 'active' : ''  ?>" href="<?php echo base_url("client/extract");  ?>">
+          <span data-feather="file"></span>
+          Extrato
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'transfer' ? 'active' : ''  ?>" href="<?php echo base_url("client/transfer"); ?>">
+          <span data-feather="shopping-cart"></span>
+          Transferência
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'pay' ? 'active' : ''  ?>" href="<?php echo base_url("client/pay"); ?>">
+          <span data-feather="shopping-cart"></span>
+          Pagar
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'investments' ? 'active' : ''  ?>" href="<?php echo base_url('client/investments'); ?> ">
+          <span data-feather="users"></span>
+          Investimentos
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'loan' ? 'active' : ''  ?>" href="<?php echo base_url('client/loans'); ?>">
+          <span data-feather="bar-chart-2"></span>
+          Empréstimo
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'card' ? 'active' : ''  ?>" href="<?php echo base_url('client/card'); ?>">
+          <span data-feather="layers"></span>
+          Cartão
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?= $page == 'renegociation' ? 'active' : ''  ?>" href="<?php echo base_url('client/renegociation') ?>">
+          <span data-feather="layers"></span>
+          Renegociação
+        </a>
+      </li>
+    </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Entre em contato conosco</span>
-          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              E-mail
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Chat
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Redes Sociais
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+      <span>Entre em contato conosco</span>
+      <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+        <span data-feather="plus-circle"></span>
+      </a>
+    </h6>
+    <ul class="nav flex-column mb-2">
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file-text"></span>
+          E-mail
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file-text"></span>
+          Chat
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <span data-feather="file-text"></span>
+          Redes Sociais
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
