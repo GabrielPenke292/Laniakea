1e4a8de5210785912d4a222a40a035ba36a0d2d2
Gabriel Penke <gabri.penke@gmail.com>
2023-12-20 21:19:31 -0300

    [virtual_world] feat: a class to create the points in the graph

---
+++ Virtual_World/js/primitives/point.js
@@ -0,0 +1,18 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(ctx, size = 18, color = "black") {
+        const rad = size / 2; // radius
+        ctx.beginPath();
+        ctx.fillStyle = color;
+
+        // this is the circle
+        ctx.arc(this.x, this.y, rad, 0, 2 * Math.PI);
+
+        ctx.fill();
+
+    }
+}
